271e7d55915d3554c2c73e20620ada4c784584f6
nexblu <farras.pramudita@gmail.com>
2024-07-01 18:24:34 +0700

    feat(reset password): add page reset password

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import EzLinkImage from '../../../assets/image/ez-link.png'
+import FormResetPassword from '@/components/FormResetPassword'
+import "react-toastify/dist/ReactToastify.css";
+
+const ResetPasswordPage = () => {
+    return (
+        <div>
+            <section className="h-screen flex items-center justify-center bg-[#CACACA]">
+                <div className="bg-[#FFFFFF] w-[95%] h-[95%] rounded-lg text-center">
+                    <div className="flex flex-wrap h-full">
+                        <div className="hidden lg:flex w-full lg:w-1/2 p-4 items-center justify-center">
+                            <Image src={EzLinkImage} alt="EZ-Link" className="h-[80%] w-[80%] rounded-lg"></Image>
+                        </div>
+                        <div className="w-full lg:w-1/2 pt-[4rem] flex flex-col items-center justify-center pe-5 ps-5">
+                            <h1 className="text-[25px] font-bold">Welcome Back !</h1>
+                            <p className="text-[12px] text-[#20201f]">Please Enter Your Email</p>
+                            <FormResetPassword />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default ResetPasswordPage
---
+++ src/components/FormResetPassword.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React, { useState } from 'react';
+
+const FormResetPassword = () => {
+    const [loading, setLoading] = useState(false)
+
+    const [email, setEmail] = useState('')
+
+    const [emailMessageError, setEmailMessageError] = useState('')
+
+    return (
+        <div>
+            <form className="w-full">
+                <label htmlFor="exampleInputemail" className="block text-sm font-medium text-gray-700 text-left">Email</label>
+                <input type="text" id="exampleInputemail" aria-describedby="emailHelp" className={`block w-full ${emailMessageError ? 'h-[2rem]' : ''} px-3 py-2 border ${!emailMessageError ? 'border-gray-300' : 'border-red-600'} rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`} value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+                {emailMessageError ? <p className='text-left text-red-600 sm:text-sm'>{emailMessageError}</p> : ''}
+                <button type="submit" className="w-full mt-3 mb-3 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">{loading ? 'Loading ...' : 'Reset Password'}</button>
+                <div className="flex flex-row justify-end text-sm">
+                    <div className="me-1">{"Don't Have Account ?"}</div>
+                    <a href="http://localhost:3000/register" className="ms-1 text-blue-500">Register</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default FormResetPassword
