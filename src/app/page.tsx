73b9e96f79786543fa4a5e38d1567f9069ac6719
nexblu <farras.pramudita@gmail.com>
2024-07-17 22:13:17 +0700

    style(home): update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import NavBar from "@/components/NavBar"
 import FormShortLink from "@/components/FormShortLink"
 
+export const metadata = {
+    title: 'Ez Link',
+}
+
 const HomePage = () => {
     return (
         <>
             <NavBar />
             <div className="bg-[#FFFFFF] max-h-screen flex items-center justify-center w-full pb-[5rem] pt-[5rem]">
-                <div className="border bg-[#CACACA] flex flex-col items-center justify-center w-[50%] rounded-lg">
+                <div className="border bg-[#CACACA] flex flex-col w-[50%] rounded-lg">
                     <FormShortLink />
                 </div>
             </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,44 +1,44 @@
+import Image from 'next/image';
+
 const NavBar = () => {
     return (
-        <>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
+        <div className="px-4 mx-auto max-w-7xl sm:px-6">
+            <div className="relative pt-6 pb-16 sm:pb-24">
+                <nav className="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
+                    <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                        <div className="flex items-center justify-between w-full md:w-auto">
+                            <a href="#" className="flex items-center">
+                                <p className='font-normal text-gray-500'>Ez Link</p>
+                            </a>
+                            <div className="flex items-center -mr-2 md:hidden">
+                                <button className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-50 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-50" type="button" aria-expanded="false">
+                                    <span className="sr-only">Open main menu</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>My URLs</a></li>
-                            <li><a>Profile</a></li>
-                        </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">Ez Link</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>My URLs</a></li>
-                        <li><a>Profile</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Logout</a>
-                </div>
+                    <div className="hidden md:flex md:space-x-10 list-none">
+                        <li>
+                            <a href="#" className="text-base font-normal text-gray-500 list-none hover:text-gray-900" target="">{"My URL's"}</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-base font-normal text-gray-500 list-none hover:text-gray-900" target="">History</a>
+                        </li>
+                    </div>
+                    <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+                        <div className="inline-flex rounded-full shadow">
+                            <a href="#" className="inline-flex items-center px-4 py-2 text-base text-gray-900 bg-white border border-transparent rounded-full cursor-pointer font-base hover:bg-gray-50">
+                                Sign in
+                            </a>
+                        </div>
+                    </div>
+                </nav>
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default NavBar
+export default NavBar;
